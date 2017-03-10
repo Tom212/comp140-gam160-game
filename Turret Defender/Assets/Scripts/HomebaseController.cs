3fb9b5116b51521921dd2f862f6238ec7a96bb53
tom212 <tomdaniels212@live.co.uk>
2017-03-10 10:34:50 +0000

    Added game prototype.

---
+++ Turret Defender/Assets/Scripts/HomebaseController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HomebaseController : MonoBehaviour {
+
+    public int homebaseHealth;
+    public int tankDamage;
+
+    public Slider healthUI;
+
+    void Start()
+    {
+
+        healthUI.maxValue = homebaseHealth;
+        healthUI.value = homebaseHealth;
+
+    }
+	
+    void OnTriggerEnter (Collider entrant)
+    {
+        
+        if (entrant.gameObject.CompareTag("Tank"))
+        {
+          
+            TankReceived(entrant.gameObject);
+
+        }
+
+    }
+
+    void TankReceived (GameObject enemy)
+    {
+
+        TakeDamage();
+        Destroy(enemy, 4f);
+
+    }
+
+    void TakeDamage ()
+    {
+
+        homebaseHealth -= tankDamage;
+        Debug.Log("Taking damage!");
+
+        healthUI.value = homebaseHealth;
+
+        if (homebaseHealth <= 0)
+        {
+            Debug.Log("Game over!");
+            SceneManager.LoadScene(0);
+        }
+
+    }
+
+}
---
+++ Turret Defender/Assets/Scripts/TankAI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankAI : MonoBehaviour {
+
+    public GameObject explosion;
+    private NavMeshAgent myAgent;
+    
+
+	// Use this for initialization
+	void Awake()
+    {
+
+        myAgent = GetComponent<NavMeshAgent>();
+        
+	}
+
+    public void SetTarget (Vector3 target) //Enemy spawner calls this when it randomly select a target.
+    {
+        myAgent.SetDestination(target);
+    }
+
+    public void Die()
+    {
+
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject, .15f);
+
+    }
+
+}
